aa858548b7a5a008f5314e34955adfe116059989
Boris Bošković <boris.boskovic92@gmail.com>
2023-09-11 22:19:57 +0200

    Neovim: remaps and tab settings

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,2 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move selection up / down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Quick fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Paste from register 0
+vim.keymap.set("n", "<leader>pp", "\"0p")
--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -1,1 +1,23 @@
 vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true -- Search highlighting
+vim.opt.incsearch = true -- Incremental search
+
+vim.opt.scrolloff = 8 -- Number of lines beneath/above the cursor
+vim.opt.signcolumn = "yes"
+
+vim.opt.colorcolumn = "80"
