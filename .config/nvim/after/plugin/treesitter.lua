12bf4079f27ba4ddfbde5eb5749804736c708d5b
Boris Bošković <boris.boskovic92@gmail.com>
2023-11-07 19:54:54 +0100

    Configured code folding with nvim_treesitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -31,3 +31,7 @@ require('nvim-treesitter.configs').setup({
         additional_vim_regex_highlighting = false,
     },
 })
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevelstart = 99
