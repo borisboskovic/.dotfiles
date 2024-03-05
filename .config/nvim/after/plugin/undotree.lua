4ad7809d8725315c3de2cde542415d485834e4c9
Boris Bošković <boris.boskovic92@gmail.com>
2024-03-05 19:18:56 +0100

    Added safe call for nvim undotree

--- .config/nvim/after/plugin/undotree.lua
+++ .config/nvim/after/plugin/undotree.lua
@@ -1,3 +1,8 @@
+local status_ok, _ = pcall(require, 'tpope/vim-fugitive')
+if not status_ok then
+    return
+end
+
 vim.keymap.set('n', '<leader>u', function()
     vim.cmd.UndotreeToggle()
     vim.cmd.UndotreeFocus()
